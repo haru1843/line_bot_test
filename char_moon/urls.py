536d7abb28b1b5e4476c66fd9ae5bb1c4bba7fce
haru <aaa@example.com>
2019-02-01 00:15:02 +0900

    add func[webhook]

--- char_moon/urls.py
+++ char_moon/urls.py
@@ -7,4 +7,5 @@ app_name = 'char_moon'
 urlpatterns = [
     url(r'^index$', views.index, name="index"),
     url(r'^callback$', views.callback, name="callback"),
+    url(r'^webhook$', views.webhook, name="webhook"),
 ]
--- char_moon/views.py
+++ char_moon/views.py
@@ -1,9 +1,14 @@
 # -*- encoding: utf-8 -*-
 
-from django.shortcuts import render
-import json
-import requests
-from django.http import HttpResponse
+# from django.shortcuts import render
+# import json
+# import requests
+# from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 # Create your views here.
 
 REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
@@ -43,3 +48,35 @@ def callback(request):
             reply += reply_text(reply_token, text)
     
     return HttpResponse(reply)
+
+line_bot_api = LineBotApi('K1OkmS/ONGZyfiSxAfcyo6lwUPwLQexJQ1NRZqveFlyIg9u7/xCYSSMh9OGxTRU58Jl6ei4ikO7gikR9/HItoB3kO5YEYw5rZIUKfd2a/cgFrqz9BHl36dZrIPGUENxrIgZ1/0E3ILFuzRo7QtDn/AdB04t89/1O/w1cDnyilFU=')
+parser = WebhookParser('c15e393c696f8ffa024eaad30a42a2db')
+
+
+@csrf_exempt
+def webhook(request):
+    if request.method != 'POST':
+        return HttpResponse('ん？なんやようか？', status=405)
+
+    signature = request.META['HTTP_X_LINE_SIGNATURE']
+    body = request.body.decode('utf-8')
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        return HttpResponseForbidden()
+    except LineBotApiError:
+        return HttpResponseBadRequest()
+
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+
+        text_send_message = TextSendMessage(text=event.message.text)
+        line_bot_api.reply_message(
+            event.reply_token,
+            text_send_message
+        )
+
+    return HttpResponse(status=200)
