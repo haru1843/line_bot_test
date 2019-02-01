6458db84b8ca82ef86a07e33657848ec4ede7bbe
haru <aaa@example.com>
2019-02-01 17:59:26 +0900

    fetch urls for char_moon

--- char_moon/urls.py
+++ char_moon/urls.py
@@ -7,5 +7,6 @@ app_name = 'char_moon'
 urlpatterns = [
     url(r'^index$', views.index, name="index"),
     url(r'^callback$', views.callback, name="callback"),
+    url(r'^char_moon$', views.char_moon, name="char_moon"),
     # url(r'^webhook$', views.webhook, name="webhook"),
 ]
