9baaf00a58f98638a2cd42c8e65e1072fcb4d57f
haru <aaa@example.com>
2019-02-02 20:35:00 +0900

    urlsにviewsのidentify_requestへのurlを追加

--- char_moon/urls.py
+++ char_moon/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^index$', views.index, name="index"),
     url(r'^callback$', views.callback, name="callback"),
     url(r'^disp_moon$', views.disp_moon, name="disp_moon"),
+    url(r'^identify_request$', views.identify_request, name="identify_request"),
     # url(r'^webhook$', views.webhook, name="webhook"),
 ]
